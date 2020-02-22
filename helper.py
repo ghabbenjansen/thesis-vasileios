dc707402108a4295f9a1d61d4be5fdaf246c0936
vserentellos <seredellos@gmail.com>
2020-02-22 20:25:40 +0100

    Added more interpretable comments

--- helper.py
+++ helper.py
@@ -3,23 +3,47 @@ from pandas.tseries.offsets import DateOffset
 
 
 def convert2flexfringe_format(win_data):
+    """
+    Function to convert the windowed data into a trace in the format accepted by the multivariate version of flexfringe
+    :param win_data: the windowed dataframe
+    :return: a list of the events in the trace with features separated by comma in each event
+    """
     return list(map(lambda x: ','.join(x), win_data.to_numpy().tolist()))
 
 
-def extract_traces(in_filepath, out_filepath, window, stride, selected):
+def extract_traces(in_filepath, out_filepath, selected, window=5, stride=1):
+    """
+    Function for extracting traces from the dataframe stored in the in_filepath and saving them in out_filepath. The
+    features to be taken into account are provided in the selected list. Each trace is extracted by rolling a window of
+    window seconds in the input data with a stride of stride seconds
+    :param in_filepath: the relative path of the input dataframe
+    :param out_filepath: the relative path of the output traces' file
+    :param selected: the features to be used
+    :param window: the window size
+    :param stride: the stride size
+    :return: creates and stores the traces' file extracted from the input dataframe
+    """
     data = pd.read_pickle(in_filepath)
+
+    # set the initial start and end dates, as well as the empty traces' list
     start_date = data['date'].iloc[0]
     end_date = start_date + DateOffset(seconds=window)
-    traces = []
+    traces = []     # list of lists
+
+    # iterate through the input dataframe until the end date is greater than the last date recorded
     while end_date < data['date'].iloc[-1]:
+        # retrieve the window of interest
         time_mask = (data['date'] >= start_date) & (data['date'] <= end_date)
         windowed_data = data[time_mask]     # TODO: not sure about its validity - To check it
+        # extract the trace of this window and add it to the traces' list
         traces += [convert2flexfringe_format(windowed_data[selected])]
+        # increment the window limits
         start_date += DateOffset(seconds=stride)
         end_date = start_date + DateOffset(seconds=window)
 
+    # create the traces' file in the needed format
     f = open(out_filepath + "training_traces.txt", "w")
-    f.write(str(len(traces)) + ' ' + '100:' + str(len(selected)))
+    f.write(str(len(traces)) + ' ' + '100:' + str(len(selected)))   # TODO: check what this 100 number is
     for trace in traces:
         f.write('1 ' + str(len(trace)) + ' 0:' + ' 0:'.join(trace))
     f.close()
--- run_flexfringe.py
+++ run_flexfringe.py
@@ -1,18 +1,28 @@
 import subprocess
 import graphviz
+import helper
 
 # TODO: change the filepath from hardcoded to input setting
 filepath = '/Users/vserentellos/Documents/dfasat/'
 
 
 def flexfringe(*args, **kwargs):
+    """
+    Wrapper function for running flexfringe in Python
+    :param args: the input arguments for flexfringe
+    :param kwargs: the keywords arguments (flags with their values) for flexfringe
+    :return: the opened dot file of the output of flexfringe
+    """
     command = ["--help"]
 
+    # collect the keyword arguments
     if len(kwargs) > 1:
         command = ["-" + key + "=" + kwargs[key] for key in kwargs]
 
+    # run flexfringe with the given arguments
     print("%s" % subprocess.run([filepath+"flexfringe", ] + command + [args[0]], stdout=subprocess.PIPE).stdout.decode())
 
+    # and open the output dot file
     try:
         with open(filepath + "outputs/final.dot") as fh:
             return fh.read()
@@ -23,6 +33,11 @@ def flexfringe(*args, **kwargs):
 
 
 def show(data):
+    """
+    Function for plotting a dot file, created after a run of flexfringe, through graphviz
+    :param data: the content of the dot file
+    :return: plots the created automaton provided in data
+    """
     if data == "":
         pass
     else:
@@ -31,8 +46,26 @@ def show(data):
 
 
 if __name__ == '__main__':
-    input_data_path = input('Give the path of the input file for flexfringe: ')
+    # set the needed filepaths
+    training_filepath = input('Give the relative path of the dataframe to be used for training: ')
+    traces_filepath = input('Give the relative filepath of the directory in which the traces in the flexfringe format '
+                            'will be saved: ')
+
+    # set the features to be used in the multivariate modelling
+    selected = ['src_port', 'dst_port', 'protocol_num', 'orig_ip_bytes', 'resp_ip_bytes', 'duration']
+
+    # extract the traces and save them in the traces_filepath - the window and the stride sizes of the sliding window
+    # can be also specified
+    helper.extract_traces(training_filepath, traces_filepath, selected)
+
+    # in case no traces_filepath has been provided (for example if it has already been created) provide one
+    if traces_filepath == '':
+        traces_filepath = input('Give the path of the input file for flexfringe: ')
+
+    # and set the flags for flexfringe
     extra_args = input('Give any flag arguments for flexfinge in a key value way separated by comma in between (e.g. '
                        'key1:value1,ke2:value2,...: ').split(',')
-    data = flexfringe(input_data_path, **dict([arg.split(':') for arg in extra_args]))
+
+    # run flexfringe to produce the automaton and plot it
+    data = flexfringe(traces_filepath, **dict([arg.split(':') for arg in extra_args]))
     show(data)
